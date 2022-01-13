2f036c7301e4e5fd63aad89b878ff4522a3b1b38
master <devgeorg222@gmail.com>
2022-01-13 23:39:02 +0200

    Updates

---
+++ precache-manifest.69341605b32c4e528d07317cde85c304.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6eab5aa9c05182cebd50f1666b81abb",
+    "url": "//yura-dotcom.github.io/quizReactApp/index.html"
+  },
+  {
+    "revision": "8fc6fb91c7b9a8e8cc51",
+    "url": "//yura-dotcom.github.io/quizReactApp/static/css/main.3459ba5c.chunk.css"
+  },
+  {
+    "revision": "e68afec1ae4b252cfd51",
+    "url": "//yura-dotcom.github.io/quizReactApp/static/js/2.a3c9b923.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "//yura-dotcom.github.io/quizReactApp/static/js/2.a3c9b923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc6fb91c7b9a8e8cc51",
+    "url": "//yura-dotcom.github.io/quizReactApp/static/js/main.74ac44de.chunk.js"
+  },
+  {
+    "revision": "b06e1959da238bed98a4",
+    "url": "//yura-dotcom.github.io/quizReactApp/static/js/runtime-main.9dbd3551.js"
+  }
+]);
